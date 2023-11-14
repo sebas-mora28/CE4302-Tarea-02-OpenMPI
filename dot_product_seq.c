a724d4c0cbf2c1bf343644a7048306aaa1c32aef
Jose Alejandro Chavarria Madriz <joalchama@gmail.com>
2023-11-13 23:22:48 -0600

    Add analysis

--- dot_product_seq.c
+++ dot_product_seq.c
@@ -4,22 +4,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 void fill_array(float *arr, size_t size)
 {
 
     for (int i = 0; i < size; i++)
     {
-        arr[i] = 1;
+        arr[i] = 1.5;
     }
 }
 
-
-int main(int argc, char **argv){
-
+int main(int argc, char **argv)
+{
 
     int n = atoi(argv[1]);
-    clock_t start, end; 
+    clock_t start, end;
     float array1[n];
     float array2[n];
     fill_array(array1, n);
@@ -27,10 +25,10 @@ int main(int argc, char **argv){
 
     int result = 0;
     start = clock();
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         result += array1[i] * array2[i];
     }
     end = clock();
     printf("Result: %d, time elapsed: %3.15f\n", result, (float)(end - start) / CLOCKS_PER_SEC);
-
 }
--- makefile
+++ makefile
@@ -12,7 +12,7 @@ dot_product_mpi:
 	@mkdir -p $(BINDIR)
 	$(MPICC) $(CFLAGS) -o $(BINDIR)/dot_product_mpi dot_product_mpi.c
 	@$(RM) -r dot_product_mpi.o	
-	mpirun -np 3 ./$(BINDIR)/dot_product_mpi $(ARGS)
+	mpirun -np 4 ./$(BINDIR)/dot_product_mpi $(ARGS)
 
 
 dot_product: 
@@ -20,5 +20,11 @@ dot_product:
 	$(CC) $(CFLAGS) -o $(BINDIR)/dot_product dot_product_seq.c
 	@$(RM) -r dot_product.o	
 	./$(BINDIR)/dot_product $(ARGS)
+
+run_seq: dot_product
+	@mkdir -p output
+	@for i in $$(seq 1 10); do \
+		./$(BINDIR)/dot_product $(ARGS) > output/output_seq_$${i}.txt; \
+	done
 
 
